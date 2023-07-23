1424dde4a7ebf17b41a6c5d6c374ab4322790c37
Muhammed <muhammedihsansolak@gmail.com>
2023-07-23 12:25:28 +0300

    DuplicateEncoder task completed

---
+++ src/codeWarsPractice/DuplicateEncoder.java
@@ -0,0 +1,39 @@
+package codeWarsPractice;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+
+public class DuplicateEncoder {
+
+    public static void main(String[] args) {
+        String str = "Success";
+        System.out.println(encode(str));
+    }
+
+    static String encode(String word){
+        word = word.toLowerCase();
+        List<String> list = new ArrayList<>(Arrays.asList(word.split("")));
+        String result = "";
+
+        for (int i = 0; i < list.size(); i++) {
+            int frequency = Collections.frequency(list,list.get(i));
+            if (frequency == 1){
+                result += "(";
+            }else {
+                result += ")";
+            }
+        }
+        return result;
+    }
+}
+/*
+The goal of this exercise is to convert a string to a new string where each character in the new string is "(" if that character appears only once in the original string, or ")" if that character appears more than once in the original string. Ignore capitalization when determining if a character is a duplicate.
+
+Examples
+"din"      =>  "((("
+"recede"   =>  "()()()"
+"Success"  =>  ")())())"
+"(( @"     =>  "))(("
+ */
