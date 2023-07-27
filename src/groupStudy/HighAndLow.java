e0705181d844a6818330f6630cbd53b31fbb46e8
Muhammed <muhammedihsansolak@gmail.com>
2023-07-27 09:16:36 +0300

    project updated

---
+++ src/groupStudy/HighAndLow.java
@@ -0,0 +1,26 @@
+package groupStudy;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+
+public class HighAndLow {
+    public static void main(String[] args) {
+        String str = "1 2 3 4 5";
+        String result = highAndLow(str);
+    }
+    public static String highAndLow(String str){
+
+        List<String> list = new ArrayList<>(Arrays.asList(str) );
+        List<Integer> numbers = new ArrayList<>();
+
+        for (String each : list) {
+            int eachNum = Integer.parseInt(each);
+            numbers.add(eachNum);
+        }
+        int max = Collections.max(numbers), min = Collections.min(numbers);
+        return max+" "+min;
+
+    }
+}
---
+++ src/groupStudy/Isogram.java
@@ -0,0 +1,30 @@
+package groupStudy;
+
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.stream.Collectors;
+
+public class Isogram {
+    public static void main(String[] args) {
+        String str = "moose";
+        boolean result = isIsogram(str);
+        System.out.println(result);
+
+        Map<String,String> map = new HashMap<>();
+
+
+
+    }
+    public static boolean isIsogram(String str){
+
+        String[] arr = str.split("");
+
+        long n = Arrays.stream(arr)
+                .distinct()
+                .count();
+
+                return str.length() == n;
+
+    }
+}
