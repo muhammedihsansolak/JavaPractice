2c30b4333a05584a969bcda60577a717bdad04bb
Muhammed <muhammedihsansolak@gmail.com>
2023-08-29 17:03:18 +0300

    mergeTwoSortedList DONE

---
+++ src/leetCodePractice/mergeTwoSortedList/ListNode.java
@@ -0,0 +1,54 @@
+package leetCodePractice.mergeTwoSortedList;
+
+public class ListNode {
+    int val;
+    ListNode next;
+
+    ListNode() {
+    }
+
+    ListNode(int val) {
+        this.val = val;
+    }
+
+    ListNode(int val, ListNode next) {
+        this.val = val;
+        this.next = next;
+    }
+
+    public ListNode mergeTwoLists(ListNode l1, ListNode l2){
+        if(l1 == null) return l2;
+        if(l2 == null) return l1;
+        if(l1.val < l2.val){
+            l1.next = mergeTwoLists(l1.next, l2);
+            return l1;
+        } else{
+            l2.next = mergeTwoLists(l1, l2.next);
+            return l2;
+        }
+    }
+
+    public ListNode mergeTwoLists2(ListNode list1, ListNode list2){
+
+        ListNode dummy = new ListNode();
+        ListNode tail = dummy;
+
+        while (list1 != null && list2 != null){
+
+            if (list1.val < list2.val){
+                tail.next = list1;
+                list1 = list1.next;
+            } else {
+                tail.next = list2;
+                list2 = list2.next;
+            }
+            tail = tail.next;
+        }
+
+        if (list1 != null) tail.next = list1;
+        else if (list2 != null) tail.next = list2;
+
+        return dummy.next;
+    }
+
+}
