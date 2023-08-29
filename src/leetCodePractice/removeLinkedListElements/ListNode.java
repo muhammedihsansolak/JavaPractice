6d8582ea3a6b2f75e7dec1b93efc9400933ba4ff
Muhammed <muhammedihsansolak@gmail.com>
2023-08-29 13:47:20 +0300

    LinkedList practice DONE

---
+++ src/leetCodePractice/removeLinkedListElements/ListNode.java
@@ -0,0 +1,33 @@
+package leetCodePractice.removeLinkedListElements;
+
+public class ListNode {
+    int val;
+    ListNode next;
+
+    ListNode() {
+    }
+
+    ListNode(int val) {
+        this.val = val;
+    }
+
+    ListNode(int val, ListNode next) {
+        this.val = val;
+        this.next = next;
+    }
+
+    //recursive solution
+
+    public ListNode removeElements(ListNode head, int val) {
+        if (head == null) return null;
+        head.next = removeElements(head.next, val);
+        return head.val == val ? head.next : head;
+    }
+
+}
+/*
+Given the head of a linked list and an integer val, remove all the nodes of the linked list that has Node.val == val, and return the new head.
+
+Input: head = [1,2,6,3,4,5,6], val = 6
+Output: [1,2,3,4,5]
+ */
