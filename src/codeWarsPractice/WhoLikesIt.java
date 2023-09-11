1daf0607f347af559b644c40e63dde1adf4a9fbf
Muhammed <muhammedihsansolak@gmail.com>
2023-09-11 16:50:52 +0300

    whoLikesIt done

---
+++ src/codeWarsPractice/WhoLikesIt.java
@@ -0,0 +1,58 @@
+package codeWarsPractice;
+
+public class WhoLikesIt {
+
+    public static void main(String[] args) {
+
+        System.out.println(whoLikesIt("Alex", "Jacob", "Mark"));
+
+    }
+
+    public static String whoLikesIt(String... names) {
+
+        if (names.length == 0) return "no one likes this";
+
+        else if (names.length == 1){
+            String str = "";
+            str += names[0] + " likes this";
+            return str;
+        }
+
+        else if (names.length == 2){
+            String str = "";
+            str += names[0] + " and ";
+            str += names[1] + " like this";
+            return str;
+        }
+
+        else if (names.length == 3){
+            String str = "";
+            str += names[0] + ", ";
+            str += names[1] + " and ";
+            str += names[2] + " like this";
+            return str;
+        }
+
+        else if (names.length > 3){
+
+            String str = "";
+            str += names[0] + ", ";
+            str += names[1] + " and "+ (names.length-2) + " others like this";
+            return str;
+
+        }
+        return "";
+    }
+
+}
+/*
+You probably know the "like" system from Facebook and other pages. People can "like" blog posts, pictures or other items. We want to create the text that should be displayed next to such an item.
+
+Implement the function which takes an array containing the names of people that like an item. It must return the display text as shown in the examples:
+
+[]                                -->  "no one likes this"
+["Peter"]                         -->  "Peter likes this"
+["Jacob", "Alex"]                 -->  "Jacob and Alex like this"
+["Max", "John", "Mark"]           -->  "Max, John and Mark like this"
+["Alex", "Jacob", "Mark", "Max"]  -->  "Alex, Jacob and 2 others like this"
+ */
