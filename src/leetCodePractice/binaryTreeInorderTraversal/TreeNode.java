f1ec252db657f7f7f12cb8013ce38e5d34aa304f
Muhammed <muhammedihsansolak@gmail.com>
2023-11-02 18:23:05 +0300

    binaryTreePostorderTraversal DONE. Some comments added to in-pre-post order traversals

--- src/leetCodePractice/binaryTreeInorderTraversal/TreeNode.java
+++ src/leetCodePractice/binaryTreeInorderTraversal/TreeNode.java
@@ -22,6 +22,24 @@ public class TreeNode {
 
     //In-Order traversal: Left -> Root -> Right
 
+    /**
+     * In-order traversal is a depth-first approach that explores the left subtree of a node before visiting the node itself and then the right subtree. This results in nodes being visited in **ascending order**, making it especially useful for binary search trees (BSTs). The in-order traversal code you provided uses an iterative approach with a stack to simulate the recursion, making it more memory-efficient.
+     * <p>
+     * "result" is an ArrayList that will store the nodes in the order of the traversal.
+     * "stack" is a Deque (usually implemented as a LinkedList) used to keep track of the nodes to be visited.
+     * "current" is a TreeNode that starts with the root of the tree.
+     * <p>
+     *     The code follows these steps:
+     *
+     * a. Initialize the current node with the root node and iterate as long as either current is not null or the stack is not empty.
+     *
+     * b. Inside the first while loop, the code pushes the current node and all its left descendants onto the stack. This process ensures that the left subtree is traversed first.
+     *
+     * c. After all left descendants have been pushed onto the stack, the code pops a node from the stack, adds it to the result list, and then moves to the right child of the current node.
+     *
+     * d. This process continues until all nodes have been traversed, and the result list contains nodes in ascending order.
+     */
+
     public static List<Integer> inorderTraversal(TreeNode root) {
         List<Integer> result = new ArrayList<>();
         Deque<TreeNode> stack = new LinkedList<>();
--- src/leetCodePractice/binaryTreePreorderTraversal/TreeNode.java
+++ src/leetCodePractice/binaryTreePreorderTraversal/TreeNode.java
@@ -25,11 +25,25 @@ public class TreeNode {
 
     //Pre-Order traversal: Root -> Left -> Right
 
+    /**
+     * result is an ArrayList to store the nodes in the order of traversal.
+     * stack is a Deque for tracking the nodes to be visited.
+     * The code follows these steps:
+     *
+     * a. If the root is null, the function returns an empty result list.
+     *
+     * b. Initialize the stack with the root node.
+     *
+     * c. Iterate while the stack is not empty.
+     *
+     * d. Inside the loop, the code pops a node from the stack, adds it to the result list, and then pushes its right and left children onto the stack.
+     *
+     * e. This ensures that nodes are visited in the node, left, right order, which is characteristic of pre-order traversal.
+     */
+
     public List<Integer> preorderTraversal(TreeNode root) {
         List<Integer> result = new ArrayList<>();
-        if (root == null) {
-            return result;
-        }
+        if (root == null) return result;
         Deque<TreeNode> stack = new LinkedList<>();
         stack.push(root);
         while (!stack.isEmpty()) {
